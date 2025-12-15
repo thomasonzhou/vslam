dc4b1c2e7719d28eddf7ba2459ea36d0c16bf50a
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-12-14 20:31:48 -0500

    Compute absolute trajectory error

--- slam_main.cpp
+++ slam_main.cpp
@@ -1,27 +1,34 @@
 #include "viz/vizlib.h"
 #include "utils/utillib.h"
 #include <string>
-#include <vector>
-#include <Eigen/Geometry>
-
-using Eigen::Isometry3d;
-using Eigen::aligned_allocator;
-using Eigen::Quaterniond;
+#include <sophus/se3.hpp>
+#include <cmath>
 
 const auto est_config = pangolin_config()
     .trajectory_color("red");
 const std::string est_traj = "../estimated.txt";
+
 const auto gt_config = pangolin_config()
     .trajectory_color("blue");
 const std::string gt_traj = "../groundtruth.txt";
 
 void compare_trajectories() {
-  poseVector estimated_poses = trajectory_from_file(est_traj);
-  trajectory_view estimated_view(est_config, estimated_poses);
+  poseVector est_poses = trajectory_from_file(est_traj);
+  trajectory_view est_view(est_config, est_poses);
+
   poseVector gt_poses = trajectory_from_file(gt_traj);
   trajectory_view gt_view(gt_config, gt_poses);
 
-  pangolin_draw({estimated_view, gt_view});
+  double mse = 0.0;
+  for (size_t i = 0; i < gt_poses.size(); ++i){
+    const double error = (gt_poses[i].inverse() * est_poses[i]).log().norm();
+    mse += error * error;
+  }
+  mse /= static_cast<double>(gt_poses.size());
+  const double rmse = std::sqrt(mse);
+  std::cout << "RMSE: " << rmse << std::endl;
+
+  pangolin_draw({est_view, gt_view});
 }
 
 int main(int argc, char** argv) {
