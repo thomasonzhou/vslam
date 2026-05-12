db0f785e3ed6ba40f2eb5c53f977b5b74e010d5e
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2026-05-11 16:25:51 -1000

    Support MacOS Bazel build (Codex)

--- slam_main.cpp
+++ slam_main.cpp
@@ -1,134 +1,77 @@
+#include <iostream>
+#include <fstream>
 #include <g2o/core/block_solver.h>
+#include <g2o/solvers/eigen/linear_solver_eigen.h>
 #include <g2o/core/optimization_algorithm_dogleg.h>
-#include <g2o/solvers/dense/linear_solver_dense.h>
 #include <g2o/core/sparse_optimizer.h>
-#include <g2o/core/robust_kernel_impl.h>
-
-#include <ceres/rotation.h>
-
-#include <sophus/se3.hpp>
-
-#include <thread>
-#include "util/bal.h"
-#include "viz/vizlib.h"
-#include "viz/g2o_viz.h"
-#include "estimation/bundle_adjustment/g2o_ba.h"
-
-using BlockSolverType = g2o::BlockSolverX;
-using LinearSolverType = g2o::LinearSolverDense<BlockSolverType::PoseMatrixType>;
-
-using estimation::bundle_adjustment::VertexIntrinsics;
-using estimation::bundle_adjustment::VertexPose;
-using estimation::bundle_adjustment::VertexPoint;
-using estimation::bundle_adjustment::EdgeProjection;
-using estimation::bundle_adjustment::kVertexIntrinsicsIdx;
-using estimation::bundle_adjustment::kVertexPoseIdx;
-using estimation::bundle_adjustment::kVertexPointIdx;
+#include <g2o/types/slam3d/types_slam3d.h>
 
 int main(int argc, char** argv) {
-  const std::string problem_path = "../problem-16-22106-pre.txt";
-  util::BALProblem bal_problem(problem_path);
+  if (argc != 2){
+    std::cout << "Second argument is *.g2o file" << std::endl;
+    return 1;
+  }
 
-  // typedef g2o::BlockSolver<g2o::BlockSolverTraits<util::bal::kPoseDim, util::bal::kTranslationDim>> BlockSolverType;
+  const std::string input_g2o_file = argv[1];
+  std::fstream fin(input_g2o_file);
+  if (!fin){
+    std::cout << input_g2o_file << " does not exist" << std::endl;
+    return 1;
+  }
 
-  auto solver = new g2o::OptimizationAlgorithmDogleg(
-    std::make_unique<BlockSolverType>(
-      std::make_unique<LinearSolverType>()
-    )
-  );
+  constexpr int kPoseDim = 6;
+  constexpr int kOdometryDim = 6;
+  typedef g2o::BlockSolver<g2o::BlockSolverTraits<kPoseDim, kOdometryDim>> BlockSolverType;
+  typedef g2o::LinearSolverEigen<BlockSolverType::PoseMatrixType> LinearSolverType;
+  auto solver = new g2o::OptimizationAlgorithmDogleg(std::make_unique<BlockSolverType>(
+    std::make_unique<LinearSolverType>()
+  ));
 
   g2o::SparseOptimizer optimizer;
   optimizer.setAlgorithm(solver);
   optimizer.setVerbose(true);
 
-  std::vector<VertexIntrinsics*> intrinsics;
-  std::vector<VertexPose*> poses;
-  intrinsics.reserve(bal_problem.num_cameras);
-  poses.reserve(bal_problem.num_cameras);
-  std::vector<VertexPoint*> points;
-  points.reserve(bal_problem.num_points);
+  int vertex_count = 0;
+  int edge_count = 0;
 
-  int id = 0;
-  for (size_t cam = 0; cam < bal_problem.num_cameras; ++cam){
-    VertexPose* v_pose = new VertexPose();
-    
-    const auto cam_data = bal_problem.camera_data(cam);
-    v_pose->setId(id++);
-    
-    Eigen::Quaterniond q;
-    ceres::AngleAxisToQuaternion<ceres::EigenQuaternionOrder>(cam_data, q.coeffs().data());
-    Sophus::SE3d pose;
-    pose.setQuaternion(q);
-    pose.translation() = Eigen::Map<const Eigen::Vector3d>(cam_data + util::bal::kRotationDim);
-    v_pose->setEstimate(pose);
-
-    poses.emplace_back(v_pose);
-    if (cam == 0){
-      poses[0]->setFixed(true);
+  std::string name;
+  int vertex_idx = 0;
+  int edge_idx0 = 0;
+  int edge_idx1 = 1;
+  while (!fin.eof()){
+    fin >> name;
+    if (name == "VERTEX_SE3:QUAT"){
+      g2o::VertexSE3 *v = new g2o::VertexSE3();
+      fin >> vertex_idx;
+      v->setId(vertex_idx);
+      v->read(fin);
+      optimizer.addVertex(v);
+      vertex_count++;
+      if(vertex_idx == 0){
+        v->setFixed(true);
+      }
+      optimizer.addVertex(v);
     }
-    optimizer.addVertex(v_pose);
-
-    VertexIntrinsics* v_k = new VertexIntrinsics();
-    v_k->setId(id++);
-    v_k->setEstimate(Eigen::Map<const Eigen::Vector3d>(cam_data+util::bal::kPoseDim));
-    // v_k->setFixed(true);
-    optimizer.addVertex(v_k);
-    intrinsics.emplace_back(v_k);
-    
-  }
-  
-  for (size_t point = 0; point < bal_problem.num_points; ++point){
-    VertexPoint* v_point = new VertexPoint();
-    v_point->setId(id++);
-    v_point->setEstimate(Eigen::Map<const Eigen::Vector3d>(bal_problem.points(point)));
-
-    v_point->setMarginalized(true);
-    optimizer.addVertex(v_point);
-    points.emplace_back(v_point);
+    else if(name == "EDGE_SE3:QUAT"){
+      g2o::EdgeSE3 *e = new g2o::EdgeSE3();
+      fin >> edge_idx0 >> edge_idx1;
+      e->setId(edge_count++);
+      e->setVertex(0, optimizer.vertices()[edge_idx0]);
+      e->setVertex(1, optimizer.vertices()[edge_idx1]);
+      e->read(fin);
+      optimizer.addEdge(e);
+    }
+    if (!fin.good()) break;
   }
 
-  for (size_t obs = 0; obs < bal_problem.num_observations; ++obs){
-    const size_t cam_idx = bal_problem.camera_idx[obs];
-    const size_t point_idx = bal_problem.point_idx[obs];
-    EdgeProjection* edge = new EdgeProjection();
-    edge->setVertex(kVertexIntrinsicsIdx, intrinsics[cam_idx]); // can change to share intrinsics across views
-    edge->setVertex(kVertexPoseIdx, poses[cam_idx]);
-    edge->setVertex(kVertexPointIdx, points[point_idx]);
-
-    edge->setMeasurement(Eigen::Map<const Eigen::Vector2d>(bal_problem.observation_data(obs)));
-    edge->setInformation(Eigen::Matrix2d::Identity());
-    edge->setRobustKernel(new g2o::RobustKernelHuber());
-    optimizer.addEdge(edge);
-  }
-
-
-  std::vector<Eigen::Vector3d> points_viz;
-  points_viz.reserve(points.size());
-  for (size_t point = 0; point < points.size(); ++point){
-    points_viz.emplace_back(points[point]->estimate());
-  }
-
-  std::vector<Eigen::Vector3d> points_viz2 = points_viz;
-
-  std::mutex points_viz2_m;
-  std::thread draw_thread([&](){
-    viz::pangolin_draw(points_viz.size(), [&](size_t i){
-      return points_viz[i];
-    }, points_viz2.size(), [&](size_t i){
-      std::lock_guard lk(points_viz2_m);
-      return points_viz2[i];
-    });}
-  );
-
+  std::cout << "read " << vertex_count << " vertices, " << edge_count << " edges" << std::endl;
   optimizer.initializeOptimization();
 
-  viz::PointUpdateAction update_action(points, points_viz2, points_viz2_m);
-  optimizer.addPostIterationAction(&update_action);
-  
-  constexpr int kIters = 20; 
-  optimizer.optimize(kIters);
+  constexpr int kOptimizeSteps = 30;
+  optimizer.optimize(kOptimizeSteps);
 
-  draw_thread.join();
+  const std::string output_file = "result_" + input_g2o_file;
+  optimizer.save(output_file.c_str());
 
   return 0;
 }
